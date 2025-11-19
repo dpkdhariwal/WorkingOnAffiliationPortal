b866746bcbdc94207feac175e6d047fa482b4533
Deepak Dhariwal <dpkdhariwal@gmail.com>
2025-11-19 10:50:50 +0530

    File Upload and View and Remove functionality developed.

---
+++ trace/text.js
@@ -0,0 +1,7 @@
+// Get all input elements of type "text"
+var inputs = document.querySelectorAll('input[type="text"]');
+
+// Loop through all input elements and set their value
+inputs.forEach(function(input) {
+    input.value = 'dffaf';
+});
